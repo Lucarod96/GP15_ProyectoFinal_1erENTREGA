95e3ec88d99704271870a464f74e0ab885d9ee5d
Luca Rodrigaño <Lucarodrigano@gmail.com>
2025-10-21 19:20:07 -0300

    Cree el proyecto

---
+++ CinemaCentro_GP15_1erEntrega/src/Vista/App.java
@@ -0,0 +1,20 @@
+package Vista;
+
+/** 
+    @author Grupo 15
+    Luis Ezequiel Sosa
+    Lucas Saidman
+    Luca Rodrigaño
+    Ignacio Rodriguez
+**/
+
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
